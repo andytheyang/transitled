873505a4d4ffbb951996be63cee562fe452d3bf5
Andy Yang <andy.yang@instacart.com>
2024-07-29 21:46:01 -0700

    copilot reformat file as object-oriented because I don't write much Python

--- main.py
+++ main.py
@@ -1,87 +1,77 @@
+import requests
+import config
+import pprint
 from collections import namedtuple
-from typing import Tuple, List
+from typing import List
 from time import time
 from math import floor
 
-import pprint
-import requests
-import config
-
 Route = namedtuple('Route', ['global_route_id', 'direction_headsign'])
-routes = [
-    # N-inbound
-    Route(global_route_id='MUNI:4567', direction_headsign='Caltrain / Ballpark'),
-    # J-inbound
-    Route(global_route_id='MUNI:4566', direction_headsign='Embarcadero Station'),
-]
 
+class TransitAPI:
+    def __init__(self, api_key: str, lat: float, lon: float):
+        self.api_key = api_key
+        self.lat = lat
+        self.lon = lon
+        self.nearby_routes_url = 'https://external.transitapp.com/v3/public/nearby_routes'
 
-def epoch_to_transit_minutes(now: int, departure_time: float):
-    return max(0, floor((departure_time - now) / 60))
-
-
-# return times in mins, boolean list for realtime, route color
-# TODO: return hint
-def get_times(data: dict, routes: List[Route]):
-    # # TODO: loop through data and grab the info
-    # for route in data['routes']:
-    #     if route['global_route_id'] == global_route_id:
-    #         for itinerary in route['itinerary']:
-
-    now = time()
-
-    # TODO sort by routes
-    formatted_data = {
-        route['global_route_id'] + '|' + itinerary['direction_headsign']:
-        {
-            'line_name': route['route_short_name'],
-            'line_color': route['route_color'],
-            'direction': itinerary['direction_headsign'],
-            'times': [
-                {
-                    'mins_until_next': epoch_to_transit_minutes(now, item['departure_time']),
-                    'is_real_time': item['is_real_time']
-                }
-                for item in itinerary['schedule_items']
-            ]
+    def get_nearby_routes(self) -> dict:
+        query_params = {
+            "lat": self.lat,
+            "lon": self.lon
         }
-        for route in data['routes']
-        for itinerary in route['itineraries']
-    }
+        headers = {
+            "apiKey": self.api_key
+        }
+        response = requests.get(self.nearby_routes_url, params=query_params, headers=headers, timeout=15)
+        response.raise_for_status()
+        return response.json()
 
-    return [formatted_data[route.global_route_id + '|' + route.direction_headsign] for route in routes]
+class TransitDataProcessor:
+    def __init__(self, routes: List[Route]):
+        self.routes = routes
 
+    @staticmethod
+    def epoch_to_transit_minutes(now: float, departure_time: float) -> int:
+        return max(0, floor((departure_time - now) / 60))
 
-# MAIN LOGIC
+    def get_times(self, data: dict) -> List[dict]:
+        now = time()
+        formatted_data = {
+            route['global_route_id'] + '|' + itinerary['direction_headsign']:
+            {
+                'line_name': route['route_short_name'],
+                'line_color': route['route_color'],
+                'direction': itinerary['direction_headsign'],
+                'times': [
+                    {
+                        'mins_until_next': self.epoch_to_transit_minutes(now, item['departure_time']),
+                        'is_real_time': item['is_real_time']
+                    }
+                    for item in itinerary['schedule_items']
+                ]
+            }
+            for route in data['routes']
+            for itinerary in route['itineraries']
+        }
+        return [formatted_data[route.global_route_id + '|' + route.direction_headsign] for route in self.routes]
 
+def main():
+    routes = [
+        Route(global_route_id='MUNI:4567', direction_headsign='Caltrain / Ballpark'),
+        Route(global_route_id='MUNI:4566', direction_headsign='Embarcadero Station'),
+    ]
 
-nearby_routes_url = 'https://external.transitapp.com/v3/public/nearby_routes'
+    api = TransitAPI(api_key=config.api_key, lat=config.lat, lon=config.lon)
+    processor = TransitDataProcessor(routes=routes)
 
-# TODO: store securely, do not check this in
-query_params = {
-    "lat": config.lat,
-    "lon": config.lon
-}
+    try:
+        data = api.get_nearby_routes()
+        lookup = processor.get_times(data)
+        pp = pprint.PrettyPrinter(indent=1)
+        pp.pprint(lookup)
+    except requests.RequestException as e:
+        print(f"HTTP Error: {e}")
 
-headers = {
-    "apiKey": config.api_key
-}
-
-# TODO: retry logic and error handling
-response = requests.get(nearby_routes_url, params=query_params, headers=headers, timeout=15)
-
-if response.status_code == 200:
-    data = response.json()
-    time_of_response = time()
-
-    # TODO: sometimes we don't get realtime data (is_real_time = False), should we cache realtime results and store for a couple mins?
-    # TODO: error handling
-    lookup = get_times(data, routes)
-
-    pp = pprint.PrettyPrinter(indent=1)
-    pp.pprint(lookup)
-else:
-    print(f"HTTP Error: {response.status_code}")
-
-
-
+if __name__ == "__main__":
+    main()
